433f5bd21bfc1fdc2c81fba77cc673526c13bbac
noel7781 <noel7781@naver.com>
2022-03-23 00:05:19 +0900

    sites

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "블로그 테스트 업로드",
-        "excerpt":"hello   hello   hello   hello  ","categories": ["C","Cpp"],
+        "excerpt":"hello!!   hello   hello   hello  ","categories": ["C","Cpp"],
         "tags": ["A","B"],
         "url": "/c/cpp/cpp/",
         "teaser": null
