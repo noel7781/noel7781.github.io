7fea7371796d59e7b3da13b15a54b55a72cb182e
noel7781 <noel7781@naver.com>
2022-03-25 00:55:37 +0900

    docker2

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,12 +5,6 @@ var store = [{
         "url": "/til/hello/",
         "teaser": null
       },{
-        "title": "블로그 업로드 테스트2",
-        "excerpt":"hello!!   hello   hello   hello  ","categories": ["til"],
-        "tags": ["new"],
-        "url": "/til/test/",
-        "teaser": null
-      },{
         "title": "도커 사용하기",
         "excerpt":"Docker 알아보기 프로그래밍 하면서 환경설정에 어려움을 겪는 경우가 많다. 나 역시도 잦은 환경설정에 지치는 경우가 많다.(특히 버전문제) 환경설정을 할 때 한번 설정해두고 그것을 이미지화 시켜서 사용한다면 환경설정에 들어가는 아까운 시간을 아낄 수 있으며 버전에 따른 문제가 발생하는 것을 방지할 수 있지 않을까? 라는 생각이 든다면 Docker를 사용해보자. 도커를 처음 쓰게...","categories": ["til","cs"],
         "tags": ["docker"],
@@ -20,6 +14,6 @@ var store = [{
         "title": "도커 사용하기 - 2",
         "excerpt":"도커 알아보기 - 2 Docker 사용기 - 2 Docker run을 실행하면 Docker는 독립적인 컨테이너에서 프로세스를 실행한다. 이 컨테이너는 독립적인 파일시스템, 네트워크, 프로세스 상태를 가지고 있다. 명령어의 형태는 다음과 같다. $ docker run [OPTIONS] IMAGE[:TAG|@DIGEST] [COMMAND] [ARG...] docker run에서는 추가적인 명령어로 실제 vm을 사용하는것 같이 attach상태 또는 백그라운드에서 작동하게 만드는 detach를...","categories": ["til","cs"],
         "tags": ["docker"],
-        "url": "/til/cs/docker2/",
+        "url": "/til/cs/d2ockers/",
         "teaser": null
       }]
