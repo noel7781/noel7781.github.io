8a81fde96737bd72d6a08651a2b9d61cc487dd9a
noel7781 <noel7781@naver.com>
2022-03-25 00:53:15 +0900

    test2

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,6 +5,12 @@ var store = [{
         "url": "/til/hello/",
         "teaser": null
       },{
+        "title": "블로그 업로드 테스트2",
+        "excerpt":"hello!!   hello   hello   hello  ","categories": ["til"],
+        "tags": ["new"],
+        "url": "/til/test/",
+        "teaser": null
+      },{
         "title": "도커 사용하기",
         "excerpt":"Docker 알아보기 프로그래밍 하면서 환경설정에 어려움을 겪는 경우가 많다. 나 역시도 잦은 환경설정에 지치는 경우가 많다.(특히 버전문제) 환경설정을 할 때 한번 설정해두고 그것을 이미지화 시켜서 사용한다면 환경설정에 들어가는 아까운 시간을 아낄 수 있으며 버전에 따른 문제가 발생하는 것을 방지할 수 있지 않을까? 라는 생각이 든다면 Docker를 사용해보자. 도커를 처음 쓰게...","categories": ["til","cs"],
         "tags": ["docker"],
