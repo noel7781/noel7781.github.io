f86e4e37800ff984347f56ae6aee5f35bf8dcb33
noel7781 <noel7781@naver.com>
2023-07-29 00:02:09 +0900

    initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: "https://noel7781.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/noel7781.github.io",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "noel7781", // Usually your GitHub org/user name.
-  projectName: "blog", // Usually your repo name.
+  projectName: "noel7781.github.io", // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: "throw",
