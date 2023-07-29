22ee74ac1cc93913d280ed9e08f324d9701c5f25
noel7781 <noel7781@naver.com>
2023-07-29 12:03:45 +0900

    todo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
             // Replace with your project's social card
             image: "img/docusaurus-social-card.jpg",
             navbar: {
-                title: "Todo",
+                title: "Todo!",
                 logo: {
                     alt: "My Site Logo",
                     src: "img/logo.svg",
